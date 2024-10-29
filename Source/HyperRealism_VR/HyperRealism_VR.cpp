9b8ac4eb17c636d81eee0f9db4fb42bdc91a0ba5
Olaf <502935@student.saxion.nl>
2024-10-29 11:13:52 +0100

    Changed to correct engine version

---
+++ Source/HyperRealism_VR/HyperRealism_VR.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HyperRealism_VR.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HyperRealism_VR, "HyperRealism_VR" );
